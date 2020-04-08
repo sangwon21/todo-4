97e48594a8637b5becc549426f1fedc796d19992
seizze <cwcwc3@gmail.com>
2020-04-08 16:07:11 +0900

    [iOS] feat : JSON Mock 데이터 추가
    associate with #8

---
+++ mobile/TodoList/TodoListTests/NetworkingTests/MockJSON.swift
@@ -0,0 +1,37 @@
+//
+//  MockJSON.swift
+//  TodoListTests
+//
+//  Created by Chaewan Park on 2020/04/08.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import Foundation
+
+let boardResponseJSON = """
+{
+   "board":
+    {
+        "list": [
+        {
+            "title": "Todo",
+            "cards": [
+            {
+                "id": "0",
+                "title": "GitHub",
+                "detail": "add, commit, push",
+                "author": "author by iOS"
+                
+            },
+            {
+                "id": "1",
+                "title": "HIG",
+                "detail": "pretty",
+                "author": "author by iOS"
+                
+            }]
+        }]
+        
+    }
+}
+"""
