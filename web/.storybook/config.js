9d5ff93da8359969cccb386d2080d4e9c6e15de9
sangwon21 <junothyme@gmail.com>
2020-04-08 16:36:34 +0900

    [FE] feat: semantic-ui 활용을 위한 설정
    why: sematic-ui에서 제공하는 아이콘을 활용하기 위하여, css 경로를
    설정했다. 아이콘을 직접 만들지 않고, 제공하는 아이콘을 활용하기로
    결정했다. cdn 방식을 사용하지 않는 이유는, 안정적으로 서비스를 제공하기
    위함이다.
    
    associate with #9

--- web/.storybook/config.js
+++ web/.storybook/config.js
@@ -1,6 +1,8 @@
 import { configure } from "@storybook/html";
 import interopRequireDefault from "@babel/runtime/helpers/interopRequireDefault";
 
+import "../src/util/semantic-ui/icon.css";
+
 function loadStories() {
   const context = require.context("../src/stories", true, /Story\.jsx$/);
 
