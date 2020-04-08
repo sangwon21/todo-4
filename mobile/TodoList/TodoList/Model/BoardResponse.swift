be3c8cbfe2c4d7a2659bb780b25cce9afbd7eb15
seizze <cwcwc3@gmail.com>
2020-04-08 15:56:09 +0900

    [iOS] feat : 전체 보드 response 모델 추가
    associate with #8

---
+++ mobile/TodoList/TodoList/Model/BoardResponse.swift
@@ -0,0 +1,26 @@
+//
+//  BoardResponse.swift
+//  TodoList
+//
+//  Created by Chaewan Park on 2020/04/08.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import Foundation
+
+struct BoardResponse: Decodable {
+    let board: Board
+}
+
+struct Board: Decodable {
+    let lists: [List]
+}
+
+struct List: Decodable {
+    let title: String
+    let cards: [Card]
+}
+
+struct Card: Decodable {
+    let id, title, detail, author: String
+}
