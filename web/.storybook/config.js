695819873e5e6350eaeee8098429217d643d25a7
sangwon21 <junothyme@gmail.com>
2020-04-08 19:13:25 +0900

    [FE] feat: reset.scss 경로 추가
    why: reset.scss를 storybook에서 활용하기 위하여, reset.scss 경로를
    추가했다.
    
    associate with #9

--- web/.storybook/config.js
+++ web/.storybook/config.js
@@ -1,7 +1,8 @@
 import { configure } from "@storybook/html";
 import interopRequireDefault from "@babel/runtime/helpers/interopRequireDefault";
 
-import "../src/util/semantic-ui/icon.css";
+import "../src/util/style/semantic-ui/icon.css";
+import "../src/util/style/reset.scss";
 
 function loadStories() {
   const context = require.context("../src/stories", true, /Story\.jsx$/);
