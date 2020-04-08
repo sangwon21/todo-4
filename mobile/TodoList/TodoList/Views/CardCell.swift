0c239069a479e608b1b1f1ff2d5a6b9815c98cea
seizze <cwcwc3@gmail.com>
2020-04-08 21:56:13 +0900

    [iOS] feat : 커스텀 셀 설정
    associate with #8

---
+++ mobile/TodoList/TodoList/Views/CardCell.swift
@@ -0,0 +1,17 @@
+//
+//  CardCell.swift
+//  TodoList
+//
+//  Created by Chaewan Park on 2020/04/08.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+class CardCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    
+}
