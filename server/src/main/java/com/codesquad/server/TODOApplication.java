cd799a3ed3b857637f903d313ccdc61bfe674ce1
Hamill210 <han95210@naver.com>
2020-04-08 11:44:43 +0900

    [BE] feat : ServerApplication 이름 변경 및 WebConfig 추가
    - 가독성을 위해 ServerApplication -> TODOApplication 으로 변경
    
    associate with #3

---
+++ server/src/main/java/com/codesquad/server/TODOApplication.java
@@ -0,0 +1,13 @@
+package com.codesquad.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TODOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TODOApplication.class, args);
+	}
+
+}
