41fec700f6b0678c78f8643b91938b0f5b4bddd1
seizze <cwcwc3@gmail.com>
2020-04-09 21:59:32 +0900

    [iOS] feat : Mock JSON 데이터 추가
    associate with #24

--- mobile/TodoList/TodoList/NetworkingTests/MockJSON.swift
+++ mobile/TodoList/TodoList/NetworkingTests/MockJSON.swift
@@ -27,6 +27,54 @@ let boardResponseJSON = """
                 "title": "HIG 공부하기",
                 "detail": "Pretty is best",
                 "author": "author by iOS"
+            },
+            {
+                "id": "4",
+                "title": "끝내주게 숨쉬기",
+                "detail": "Pretty is best",
+                "author": "author by iOS"
+            },
+            {
+                "id": "5",
+                "title": "간지나게 자기",
+                "detail": "Pretty is best",
+                "author": "author by iOS"
+            },
+            {
+                "id": "6",
+                "title": "작살나게 밥먹기",
+                "detail": "Pretty is best",
+                "author": "author by iOS"
+            },
+            {
+                "id": "7",
+                "title": "메일 보내기",
+                "detail": "Pretty is best",
+                "author": "author by iOS"
+            },
+            {
+                "id": "8",
+                "title": "수업 듣기",
+                "detail": "Pretty is best",
+                "author": "author by iOS"
+            },
+            {
+                "id": "9",
+                "title": "집에 가서 자기",
+                "detail": "Pretty is best",
+                "author": "author by iOS"
+            },
+            {
+                "id": "10",
+                "title": "코드스쿼드 출근하기",
+                "detail": "Pretty is best",
+                "author": "author by iOS"
+            },
+            {
+                "id": "11",
+                "title": "블로그 도메인 연결하기",
+                "detail": "Pretty is best",
+                "author": "author by iOS"
             }]
         },
         {
@@ -36,6 +84,12 @@ let boardResponseJSON = """
                 "id": "2",
                 "title": "기획서 리뷰",
                 "detail": "투두 리스트 만들기\\n- 새 카드 추가하기\\n- 카드 삭제하기",
+                "author": "author by web"
+            },
+            {
+                "id": "12",
+                "title": "공부",
+                "detail": "과연 할까?",
                 "author": "author by web"
             }]
         },
