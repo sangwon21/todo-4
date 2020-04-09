5fabda80aa62f99509ea6e06ddde006d9bb2b4a6
sangwon21 <junothyme@gmail.com>
2020-04-09 20:07:16 +0900

    [FE] feat: 프로젝트 전체 entry point 설정
    why: 프로젝트를 webpack으로 번들링(bundling)하기 위한 진입점(entry
    point)를 설정했다. 이를 위해, 제작한 컴포넌트를 조합했다.
    
    associate with #16

--- web/src/component/Board/index.ts
+++ web/src/component/Board/index.ts
@@ -2,10 +2,17 @@ import { Table } from "../Table";
 import { InlineList, InlineListClass } from "../../styled-component/InlineList";
 
 export class Board {
-  constructor() {}
+  private boardClass: string;
+  constructor(boardClass: string) {
+    this.boardClass = boardClass;
+  }
 
   render() {
-    return InlineList({ class: InlineListClass.DEFAULT, width: "100%" })([
+    return InlineList({
+      class: InlineListClass.SPACE_AROUND,
+      width: "100%",
+      userClassList: [this.boardClass],
+    })([
       new Table("할 일").render(),
       new Table("하는 중").render(),
       new Table("한 일").render(),
---
+++ web/src/index.ts
@@ -0,0 +1,14 @@
+import { div } from "wonnie-template";
+import { Board } from "./component/Board";
+import { Header } from "./component/Header";
+
+import "./index.scss";
+import "./util/style/semantic-ui/icon.css";
+import "./util/style/reset.scss";
+
+const board = new Board("test").render();
+
+const root = div({ id: "root" })();
+const body = document.querySelector("body")!;
+body.appendChild(root);
+root.appendChild(div()([Header, board]));
