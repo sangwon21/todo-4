487554c651dc6b6ea99aaec95bdc7d68f5d35b79
sangwon21 <junothyme@gmail.com>
2020-04-09 14:06:09 +0900

    [FE] refactor: Card 컴포넌트 Class 형식으로 교체
    why: Card 컴포넌트를 개발할 때, 함수 형식 컴포넌트 개발 방식으로 한계를
    보였다. 컴포넌트와 이벤트, 컴포넌트와 데이터 등 결합도가 낮아, Class
    방식으로 개발하여, 결합도를 높이고자 하였다.
    
    associate with #12

--- web/src/component/Card/index.ts
+++ web/src/component/Card/index.ts
@@ -3,24 +3,44 @@ import { InlineList, InlineListClass } from "../../styled-component/InlineList";
 
 import "./card.scss";
 
-const rightHeader = InlineList({ class: InlineListClass.DEFAULT })([
-  i({ class: "tasks icon" })(),
-  div({ class: "card-title" })(["페이지네이션 UI 리서치"]),
-]);
+export class Card {
+  private contents: string;
+  private cardNode: Element | Text | null;
+  constructor(contents: string) {
+    this.contents = contents;
+    this.cardNode = null;
+    this.closeButtonHandler = this.closeButtonHandler.bind(this);
+  }
 
-const closeButton = div()([i({ class: "close icon" })()]);
+  closeButtonHandler() {
+    this.cardNode && this.cardNode.remove();
+  }
 
-const header = InlineList({
-  class: InlineListClass.SPACE_BETWEEN,
-  width: "100%",
-})([rightHeader, closeButton]);
+  render() {
+    const rightHeader = InlineList({ class: InlineListClass.DEFAULT })([
+      i({ class: "tasks icon" })(),
+      div({ class: "card-title" })([this.contents]),
+    ]);
 
-const footer = InlineList({ class: InlineListClass.DEFAULT })([
-  span({ class: "card-prefix" })(["Added by"]),
-  span({ class: "card-author" })(["nigayo"]),
-]);
+    const closeButton = div({ onClick: this.closeButtonHandler })([
+      i({ class: "close icon" })(),
+    ]);
 
-export const Card = InlineList({
-  class: InlineListClass.SPACE_BETWEEN_COLUMN,
-  userClassList: ["card"],
-})([header, footer]);
+    const header = InlineList({
+      class: InlineListClass.SPACE_BETWEEN,
+      width: "100%",
+    })([rightHeader, closeButton]);
+
+    const footer = InlineList({ class: InlineListClass.DEFAULT })([
+      span({ class: "card-prefix" })(["Added by"]),
+      span({ class: "card-author" })(["nigayo"]),
+    ]);
+
+    this.cardNode = InlineList({
+      class: InlineListClass.SPACE_BETWEEN_COLUMN,
+      userClassList: ["card"],
+    })([header, footer]);
+
+    return this.cardNode;
+  }
+}
