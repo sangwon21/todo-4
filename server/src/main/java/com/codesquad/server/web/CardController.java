c009c03b9b4db4384020e3d3069b9eadf3ff7821
Hamill210 <han95210@naver.com>
2020-04-09 16:58:46 +0900

    [BE] feat : domain, repository, web 폴더 안 클래스 구현
    - domain : User, Column, Card 클래스
    - web : UserController, ColumnController, CardController
    - repository : UserRepository, ColumnRepository, CardRepository
    
    associate with #17

--- server/src/main/java/com/codesquad/server/web/CardController.java
+++ server/src/main/java/com/codesquad/server/web/CardController.java
@@ -1,4 +1,10 @@
 package com.codesquad.server.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/columns/{id}/cards")
 public class CardController {
+
 }
