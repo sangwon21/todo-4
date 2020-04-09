a9da4466a9062707588517dbd518b055851522d2
sangwon21 <junothyme@gmail.com>
2020-04-09 14:09:08 +0900

    [FE] refactor: AddingCard 컴포넌트 Class 방식으로 교체
    why: Card 컴포넌트를 교체한 이유와 마찬가지로 AddingCard 컴포넌트도
    Class 방식으로 교체했다.
    
    associate with #12

--- web/src/component/AddingCard/index.ts
+++ web/src/component/AddingCard/index.ts
@@ -1,38 +1,59 @@
 import { div, i, textarea } from "wonnie-template";
-import { CustomSubmitButton, ButtonSize, ButtonType } from "../CustomButton";
+import { CustomButton, ButtonSize, ButtonType } from "../CustomButton";
 import COLOR from "../../util/color";
 import { InlineList, InlineListClass } from "../../styled-component/InlineList";
 
 import "./AddingCard.scss";
 
-const cardTextarea = div({ style: "width: 100%; height: 50%;" })([
-  textarea({
-    class: "textarea card-textarea",
-  })(),
-]);
+export interface AddingCardParam {
+  rightButtonType?: ButtonType;
+  leftButtonType?: ButtonType;
+  rightButtonCallback?: Function;
+  leftButtonCallback?: Function;
+  userClass: string;
+}
 
-const rightButtonType: ButtonType = {
-  size: ButtonSize.large,
-  color: COLOR.SECONDARY,
-  content: "Cancel",
-  contentColor: COLOR.FONT,
-};
+export class AddingCard {
+  private rightButtonType: ButtonType;
+  private leftButtonType: ButtonType;
+  private userClass: string;
 
-const leftButtonType: ButtonType = {
-  size: ButtonSize.large,
-  color: COLOR.PRIMARY,
-  content: "Add",
-  contentColor: COLOR.WHITE,
-};
+  constructor(param: AddingCardParam | void) {
+    this.rightButtonType = (param && param.rightButtonType) || {
+      size: ButtonSize.large,
+      color: COLOR.SECONDARY,
+      content: "Cancel",
+      contentColor: COLOR.FONT,
+      callback: (param && param.rightButtonCallback) || undefined,
+    };
+    this.leftButtonType = (param && param.leftButtonType) || {
+      size: ButtonSize.large,
+      color: COLOR.PRIMARY,
+      content: "Add",
+      contentColor: COLOR.WHITE,
+      callback: (param && param.leftButtonCallback) || undefined,
+    };
 
-const buttons = InlineList({
-  class: InlineListClass.SPACE_BETWEEN,
-  width: "100%",
-})([CustomSubmitButton(leftButtonType), CustomSubmitButton(rightButtonType)]);
+    this.userClass = param ? param.userClass : "";
+  }
 
-export const AddingCard = InlineList({
-  class: InlineListClass.SPACE_BETWEEN_COLUMN,
-  width: "100%",
-  height: "8rem",
-  userClassList: ["adding-card"],
-})([cardTextarea, buttons]);
+  render() {
+    const buttons = InlineList({
+      class: InlineListClass.SPACE_BETWEEN,
+      width: "100%",
+    })([CustomButton(this.leftButtonType), CustomButton(this.rightButtonType)]);
+
+    const cardTextarea = div({ style: "width: 100%; height: 50%;" })([
+      textarea({
+        class: "textarea card-textarea",
+      })(),
+    ]);
+
+    return InlineList({
+      class: InlineListClass.SPACE_BETWEEN_COLUMN,
+      width: "100%",
+      height: "8rem",
+      userClassList: ["adding-card", this.userClass],
+    })([cardTextarea, buttons]);
+  }
+}
