86bca9eb43369559be341576f7f7e0913e29f3a4
seizze <cwcwc3@gmail.com>
2020-04-09 20:08:36 +0900

    [iOS] test : 테스트코드 작성할 파일 세팅
    associate with #22

---
+++ mobile/TodoList/TodoListTests/CardListViewModelTests.swift
@@ -0,0 +1,18 @@
+//
+//  CardListViewModelTests.swift
+//  TodoListTests
+//
+//  Created by Chaewan Park on 2020/04/06.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import XCTest
+@testable import TodoList
+
+class CardListViewModelTests: XCTestCase {
+    
+    override func setUp() {
+        
+        
+    }
+}
