a8c21770c016c9cf5f12058628d364ba387931cf
Hamill210 <han95210@naver.com>
2020-04-10 14:41:21 +0900

    [BE] refactor : 실서버 주소 삽입과 CORS 문제 해결을 위한 애노테이션 추가
    - 실서버 주소 : 52.207.159.215:3306
    - @CrossOrigin("*")
    
    associate with #19

--- server/src/main/java/com/codesquad/server/config/WebConfig.java
+++ server/src/main/java/com/codesquad/server/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.codesquad.server.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@CrossOrigin("*")
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
