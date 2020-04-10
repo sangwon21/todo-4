c21471699f33e1c48eb04ee7fbf34ece79af09be
seizze <cwcwc3@gmail.com>
2020-04-10 15:15:38 +0900

    [iOS] feat : 키보드에 따라서 스크롤뷰 조정
    associate with #23

--- mobile/TodoList/TodoList/Controllers/FormViewController.swift
+++ mobile/TodoList/TodoList/Controllers/FormViewController.swift
@@ -9,8 +9,36 @@
 import UIKit
 
 class FormViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    private let observers = Observers()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addKeyboardObservers()
+    }
+    
+    deinit {
+        observers.removeObservers()
+    }
+    
+    private func addKeyboardObservers() {
+        observers.addKeyboardShowObserver { [weak self] keyboardFrame in
+            let contentInset = UIEdgeInsets(
+                top: 0.0,
+                left: 0.0,
+                bottom: keyboardFrame.size.height,
+                right: 0.0)
+            self?.scrollView.contentInset = contentInset
+            self?.scrollView.scrollIndicatorInsets = contentInset
+        }
+        
+        observers.addKeyboardHideObserver { [weak self] in
+            let contentInset = UIEdgeInsets.zero
+            self?.scrollView.contentInset = contentInset
+            self?.scrollView.scrollIndicatorInsets = contentInset
+        }
     }
 }
