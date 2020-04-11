f443861faddfef24274de6291ee0a0a723bfa7bc
ever <hsik0225@gmail.com>
2020-04-11 19:31:15 +0900

    [BE] feat : UserService 클래스 생성
    - User의 회원가입, 로그인 기능을 가지고 있다
    - SignUpRequest 클래스는 필요없다 생각하여 삭제
    
    associate with #21

--- server/src/main/java/com/codesquad/server/service/UserService.java
+++ server/src/main/java/com/codesquad/server/service/UserService.java
@@ -1,5 +1,6 @@
 package com.codesquad.server.service;
 
+import com.codesquad.server.domain.User;
 import com.codesquad.server.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,8 +11,25 @@ public class UserService {
 
     private UserRepository userRepository;
 
-    public void signUp(SignUpRequest signUpRequest) {
-        
+    public User signUp(User requestUser) {
+        verifyDuplicatedUser(requestUser.getUserId());
+        return userRepository.save(requestUser);
+    }
 
+    private void verifyDuplicatedUser(String userId) {
+        userRepository.findUserByUserId(userId).orElseThrow(() -> new IllegalArgumentException("중북된 유저입니다!"));
+    }
+
+    private User findUserByUserId(String userId) {
+        return userRepository.findUserByUserId(userId).orElseThrow(() -> new IllegalArgumentException("없는 유저입니다!"));
+    }
+
+    public User siginIn(User requestUser) {
+        User user = findUserByUserId(requestUser.getUserId());
+
+        if(!user.getPassword().equals(requestUser.getPassword())) {
+            throw new IllegalArgumentException("암호가 일치하지 않습니다.");       }
+
+        return user;
     }
 }
