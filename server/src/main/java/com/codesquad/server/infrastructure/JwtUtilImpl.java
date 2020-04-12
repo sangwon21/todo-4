65a332a643379b794589f623a14a6b05caf5a23d
ever <hsik0225@gmail.com>
2020-04-12 21:29:30 +0900

    [BE] refactor : encryptor.properties 생성
    - 암호화 해야 하는 내용들을 encryptor.properties로 이동

--- server/src/main/java/com/codesquad/server/infrastructure/JwtUtilImpl.java
+++ server/src/main/java/com/codesquad/server/infrastructure/JwtUtilImpl.java
@@ -4,10 +4,12 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.codesquad.server.domain.service.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class JwtUtilImpl implements JwtUtil {
 
     @Value("${jwt.signKey}")
@@ -22,6 +24,10 @@ public class JwtUtilImpl implements JwtUtil {
     private String issuer = "code-squad.com";
 
     public String createToken() {
+        log.info("userId : {}", userId);
+        log.info("password : {}", password);
+        log.info("signKey : {}", signKey);
+
          return JWT.create()
                 .withIssuer(issuer)
                 .withSubject(userId)
