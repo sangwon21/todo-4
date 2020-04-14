91af1b835df9efc71eea22332ed631198b2fe9c9
sangwon21 <junothyme@gmail.com>
2020-04-14 12:21:04 +0900

    [FE] feat: 실패할 경우 알려주는 모달 추가
    why: 서버와 데이터 전송 중에, 실패하는 경우, 사용자에게 실패한 사실을
    알려주는 모달을 추가했다. 이를 통해, 사용자는 자신의 행동이 반영되지
    않았다는 사실을 인지할 수 있다.
    
    associate with #25

---
+++ web/src/component/Table/Card/FailModal/index.ts
@@ -0,0 +1,56 @@
+import { div, i, span } from "wonnie-template";
+import { Modal } from "../../../Modal";
+import { CustomButton, ButtonSize, ButtonType } from "../../../CustomButton";
+import COLOR from "../../../../util/color";
+import {
+  InlineList,
+  InlineListClass,
+} from "../../../../styled-component/InlineList";
+
+import "./FailModal.scss";
+
+export class FailModal {
+  private failModalNode: Element | Text | null = null;
+  constructor() {
+    this.close = this.close.bind(this);
+  }
+
+  close() {
+    this.failModalNode!.remove();
+  }
+
+  render() {
+    const leftHeader = span({ class: "fail-modal-header-title" })(["실패"]);
+    const rightHeader = i({ class: "close icon", onClick: this.close })();
+
+    const header = InlineList({
+      class: InlineListClass.SPACE_BETWEEN,
+      width: "100%",
+      userClassList: ["fail-modal-header"],
+    })([leftHeader, rightHeader]);
+
+    const contents = div({ class: "fail-modal-text" })([
+      "다시 한 번 시도해주세요",
+    ]);
+
+    const mediumSettings: ButtonType = {
+      size: ButtonSize.medium,
+      color: COLOR.PRIMARY,
+      content: "close",
+      contentColor: COLOR.WHITE,
+      callback: this.close,
+    };
+
+    const closeButton = InlineList({
+      class: InlineListClass.ALIGN_RIGHT,
+      userClassList: ["fail-modal-button"],
+    })([CustomButton(mediumSettings)]);
+
+    const ModalContent = InlineList({
+      class: InlineListClass.SPACE_BETWEEN_COLUMN,
+      userClassList: ["fail-modal-content"],
+    })([header, contents, closeButton]);
+    this.failModalNode = Modal(ModalContent);
+    return this.failModalNode;
+  }
+}
