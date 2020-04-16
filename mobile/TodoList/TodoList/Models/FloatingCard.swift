8e6188b18726beef9ba6d1df0b18f6c567e410dc
seizze <cwcwc3@gmail.com>
2020-04-16 18:01:46 +0900

    [iOS] feat : Drag 상태의 카드 모델 추가
    Associate with #37

---
+++ mobile/TodoList/TodoList/Models/FloatingCard.swift
@@ -0,0 +1,15 @@
+//
+//  FloatingCard.swift
+//  TodoList
+//
+//  Created by Chaewan Park on 2020/04/16.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import Foundation
+
+struct FloatingCard {
+    let sourceListID: Int?
+    let sourceIndex: Int
+    let card: Card
+}
