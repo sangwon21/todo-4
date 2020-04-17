800e1d03e4720245ca6453506cd616eaef79139a
sangwon21 <junothyme@gmail.com>
2020-04-17 15:52:16 +0900

    [FE] feat: 초기 데이터 받아오는 작업 추가
    why: 초기 데이터를 받아오는 작업을 추가했다. 받아온 데이터를 통해, 초기
    데이터를 컴포넌트 별로 렌더링한다.
    
    associate with #45

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,12 +1,12 @@
 import { div } from "wonnie-template";
 import { Board } from "./component/Board";
 import { Header } from "./component/Header";
+import axios from "axios";
+import API from "./util/api";
 
 import "./index.scss";
 import "./util/style/semantic-ui/icon.css";
 import "./util/style/reset.scss";
-
-const board = new Board().render();
 
 const root = div({ id: "root" })();
 const body = document.querySelector("body")!;
@@ -15,5 +15,17 @@ const mount = (to: Element, target: Element) => {
   to.appendChild(target);
 };
 
-mount(body, root as Element);
-mount(root as Element, div()([new Header().render(), board]) as Element);
+const getInitalData = async () => {
+  const {
+    data: { columns, history },
+  } = await axios.get(API.GET_CARD_AND_HISTORY_DATA);
+  const board = new Board(columns).render();
+
+  mount(body, root as Element);
+  mount(
+    root as Element,
+    div()([new Header(history).render(), board]) as Element
+  );
+};
+
+getInitalData();
